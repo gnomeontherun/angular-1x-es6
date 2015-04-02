13348e214501c3cfa8ac97113f0fbe32a6aa0ff0
Jeremy Wilken <Jeremy.Wilken@teradata.com>
2015-04-02 14:59:19 -0500

    add comments into services

--- app/services/commits-class.js
+++ app/services/commits-class.js
@@ -1,20 +1,32 @@
 'use strict';
 
+// Commit service implemented as a class.
+// This class creates a basic HTTP resource for Angular commit messages from Github. The most annoying aspect of the
+// class structure is the number of places that dependencies have to be declared or passed around.
 class CommitsClass {
+
+  // Create a constructor, and it sets the default url. The `$http` service is passed by Angular to the constructor
+  // and we have to do something with it to make it available outside of the constructor scope, so we attach to `this`.
   constructor($http) {
     this.url = 'https://api.github.com/repos/angular/angular/commits';
     this.$http = $http;
   }
 
+  // The `get` method makes an HTTP call, but also has the handy default value for page if it is not specified.
   get(page = 1) {
+    // We use the arrow function to handle the resolution callback here.
     return this.$http.get(this.url, {page: page}).then(result => result.data);
   }
 
+  // Create the static factory method so that Angular has something to call to instantiate this class, and requires us
+  // to pass the depdenencies once again.
   static factory($http) {
     return new CommitsClass($http);
   }
 }
 
+// One final step is to annotate our dependency injections for minification.
 CommitsClass.factory.$inject = ['$http'];
 
+// Export the class for the world.
 export default CommitsClass;
--- app/services/commits-proto.js
+++ app/services/commits-proto.js
@@ -1,16 +1,24 @@
 'use strict';
 
+// The `Commits` service implemented as a function object.
+// There is no real magic here, the function returns an object with the service methods for getting the Angular
+// Github commits. We declare the dependencies here and in the annotation at the end.
 function CommitsProto($http) {
 
+  // Use `let` to make this variable local to this block scope
   let url = 'https://api.github.com/repos/angular/angular/commits';
 
-  function CommitsProto() {}
+  // Create a new Commits object.
+  function Commits() {}
 
-  CommitsProto.get = function(page) {
+  // Add the `get` method, and use the injected `$http` service.
+  Commits.get = function(page) {
     return $http.get(url, {page: page}).then(result => result.data);
   }
 
-  return CommitsProto;
+  // Return the service to the factory provider.
+  return Commits;
 }
 
+// Export the commits, and use the array annotation method to make minification work.
 export default ['$http', CommitsProto];
