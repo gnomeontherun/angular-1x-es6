418df206fd1e0e25cdd5a0700bae23baf0e93251
Jeremy Wilken <Jeremy.Wilken@teradata.com>
2015-04-02 14:10:11 -0500

    make examples side by side

--- app/app.js
+++ app/app.js
@@ -3,8 +3,6 @@ import { GithubCommitsClass } from 'app/components/github-commits-class';
 import GithubCommitsProto from 'app/components/github-commits-proto';
 import { Commits } from 'app/services/commits';
 
-console.log(GithubCommitsProto);
-
 angular.module('App', [])
   .factory('Commits', Commits.factory)
   .directive('githubCommitsClass', GithubCommitsClass.factory)
